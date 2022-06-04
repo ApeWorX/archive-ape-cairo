b71d4aad77102cacefb9741bf7bce995b92ed401
unparalleled-js <yingthi@live.com>
2022-06-04 15:37:47 -0500

    feat: dependencies

--- ape_cairo/__init__.py
+++ ape_cairo/__init__.py
@@ -1,6 +1,11 @@
 from ape import plugins
 
-from .compiler import CairoCompiler
+from .compiler import CairoCompiler, CairoConfig
+
+
+@plugins.register(plugins.Config)
+def config_class():
+    return CairoConfig
 
 
 @plugins.register(plugins.CompilerPlugin)
