409d67f23a2a91c98041e17a79cf93c638850921
Juliya Smith <yingthi@live.com>
2022-06-22 10:39:33 -0500

    refactor: use splitlines
    Co-authored-by: 0xca11ab1e <105989135+ca11ab1e@users.noreply.github.com>

--- ape_cairo/compiler.py
+++ ape_cairo/compiler.py
@@ -15,11 +15,11 @@ class CairoConfig(PluginConfig):
 
 
 def _has_execute_method(contract_path: Path) -> bool:
-    for line in Path(contract_path).read_text().split("\n"):
-        if line.startswith("func __execute__{"):
-            return True
-
-    return False
+    content = Path(contract_path).read_text(encoding="utf-8")
+    return any(
+        line.startswith("func __execute__{")
+        for line in content.splitlines()
+    )
 
 
 class CairoCompiler(CompilerAPI):
